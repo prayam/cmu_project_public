82392580db03d49342743e0821ed7ed28ef7df30
jh.ahn <eidola7@gmail.com>
2021-06-23 14:42:11 +0900

    [penetration] decrypt script
    update script, extract data from db directly

--- phase2/docs/02_env_cipher_penetration/cipher.sh
+++ phase2/docs/02_env_cipher_penetration/cipher.sh
@@ -1,17 +1,25 @@
-#hexdump -e '16/1 "%02x"' /var/shinpark/secret.key
-#AES_ROOT_KEY=$?
+
+AES_ROOT_KEY=$(hexdump -e '16/1 "%02x"' /var/shinpark/secret.key)
 #echo ${AES_ROOT_KEY}
-#IV_VALUE='000000000000000000000000000000000'
+IV_VALUE='00000000000000000000000000000000'
 #echo ${IV_VALUE}
 
-echo -n ${2} | xxd -r -p > name${1}
+#extract name data
+FACE_DB_PATH=/home/lg/team6/tartan/LgFaceRecDemoTCP_Jetson_NanoV2/tartan_faces.db
+SQL_STRING="select (name) from names where id="${1}
+NAME_STRING=$(sqlite3 ${FACE_DB_PATH} "${SQL_STRING}")
 
+echo -n ${NAME_STRING} | xxd -r -p > name${1}
+
+# openssl enc -aes-128-cbc -d -in name${1} -out name${1}.dec\
+# 	-K '123456789ABCDEF03456789ABCDEF012'\
+# 	-iv '00000000000000000000000000000000'\
+# 	-nosalt -nopad
 openssl enc -aes-128-cbc -d -in name${1} -out name${1}.dec\
-	-K '123456789ABCDEF03456789ABCDEF012'\
-	-iv '00000000000000000000000000000000'\
+	-K ${AES_ROOT_KEY}\
+	-iv ${IV_VALUE}\
 	-nosalt -nopad
 
-echo "id num: "${1};
 cat name${1}.dec
-echo;
+echo "  (id_num:"${1}")";
 
--- phase2/docs/02_env_cipher_penetration/decript_blob.sh
+++ phase2/docs/02_env_cipher_penetration/decript_blob.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 
-#hexdump -e '16/1 "%02x"' /var/shinpark/secret.key
-#AES_ROOT_KEY=$?
+AES_ROOT_KEY=$(hexdump -e '16/1 "%02x"' /var/shinpark/secret.key)
 #echo ${AES_ROOT_KEY}
-#IV_VALUE='000000000000000000000000000000000'
+IV_VALUE='00000000000000000000000000000000'
 #echo ${IV_VALUE}
 
 #extract blob data
 FACE_DB_PATH=/home/lg/team6/tartan/LgFaceRecDemoTCP_Jetson_NanoV2/tartan_faces.db
 SQL_STRING="select writefile('blob.bin', face) from faces where id="${1}
-#sqlite3 tartan_faces.db "select writefile('blob10.bin', face) from faces where id=10"
 sqlite3 ${FACE_DB_PATH} "${SQL_STRING}"
-#sqlite3 /home/lg/team6/tartan/LgFaceRecDemoTCP_Jetson_NanoV2/tartan_faces.db "${SQL_STRING}"
 
 mv blob.bin blob${1}.bin
 # just eliminate first 16byte, it's for size variables
@@ -23,8 +20,11 @@ truncate -s -8 blob${1}.mod
 # or for test, you can attach 8byte 00s... It's required for AES block length matching
 #truncate -s +8 ${2}.mod
 
-openssl enc -aes-128-cbc -d -in blob${1}.mod -out blob${1}.dec\
-	-K '123456789ABCDEF03456789ABCDEF012'\
-	-iv '00000000000000000000000000000000'\
+# openssl enc -aes-128-cbc -d -in blob${1}.mod -out blob${1}.dec\
+# 	-K '123456789ABCDEF03456789ABCDEF012'\
+# 	-iv '00000000000000000000000000000000'\
+# 	-nosalt -nopad
+openssl enc -aes-128-cbc -d -in name${1} -out name${1}.dec\
+	-K ${AES_ROOT_KEY}\
+	-iv ${IV_VALUE}\
 	-nosalt -nopad
-
